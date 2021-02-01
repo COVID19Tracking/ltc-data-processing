747ab36e387ac10e7ab986bdc2ec6f33242a0dce
Josh Zarrabi <jzarrabi@vmware.com>
2021-01-31 23:24:55 -0500

    adds api endpoint to close new mexico and arkansas outbreaks
    - also adds a test

--- app/api/ltc.py
+++ app/api/ltc.py
@@ -109,6 +109,65 @@ def collapse_outbreak_rows(df):
     return processed_df
 
 
+def add_info(record, last_collected, all_data, current_date):
+    k = str(record['State']) + str(record['County']) + str(record['City']) + str(record['Facility'])
+    if k in last_collected:
+        lr = last_collected[k]
+        if type(record['County']) is not float and type(record['City']) is not float:
+            row = all_data.loc[ (all_data['Date'] == lr) & (all_data['Facility'] == record['Facility']) & (all_data['County'] == record['County'])  & (all_data['City'] == record['City'] )]
+        else:
+            row = all_data.loc[ (all_data['Date'] == lr) & (all_data['Facility'] == record['Facility'] )]
+        record = copy_row(record, row)
+
+        record['last_recorded'] = lr
+        record['Date'] = current_date
+        record['Outbrk_Status'] = 'Closed'
+    else:
+        record['last_recorded'] = "Never"
+
+    return record
+
+def add_last_reported_now(record, date):
+    record['last_recorded'] = date
+    return record
+
+def copy_row(new_row, old_row):
+    for c in old_row.columns:
+        new_row[c] = old_row.iloc[0][c]
+    return new_row
+
+def close_outbreaks(df):
+    filled_in_state = pd.DataFrame()
+
+    df = df[df['Date'].notna()]
+
+    collection_dates = df[['Date']].drop_duplicates()
+    facilities = df[['State', 'County', 'City', 'Facility']].drop_duplicates()
+
+    last_collected = { }
+
+    for date_index, date_row in collection_dates.iterrows():
+        collection_date = date_row['Date']
+        current_block = df.loc[df['Date'] == collection_date]
+        for _, block_row in current_block.iterrows():
+            k = str(block_row['State']) + str(block_row['County']) + str(block_row['City']) + str(block_row['Facility'])
+            last_collected[k] = collection_date
+
+        not_in_block = pd.merge(current_block, facilities, on = ['State', 'County', 'City', 'Facility'], how = 'right', indicator=True).loc[lambda x : x['_merge']=='right_only']
+        not_in_block = not_in_block.apply(add_info, axis = 1, args = (last_collected, df, collection_date))
+        not_in_block = not_in_block[not_in_block['last_recorded'] != "Never"]
+
+        current_block = current_block.apply(add_last_reported_now, axis = 1, args = (collection_date, ) )
+        not_in_block = not_in_block.drop(columns = ["_merge"])
+
+        filled_in_state = filled_in_state.append(current_block)
+        filled_in_state = filled_in_state.append(not_in_block)
+
+    filled_in_state = filled_in_state.convert_dtypes()
+    filled_in_state.reset_index(drop=True, inplace=True)
+    return filled_in_state
+
+
 ####################################################################################################
 #######################################   API endpoints    #########################################
 ####################################################################################################
@@ -151,6 +210,25 @@ def aggregate_outbreaks():
 
     # this will go into the lambda logs
     flask.current_app.logger.info('Collapsing %s data took %.1f seconds, %d to %d rows' % (
+        state, (t2 - t1), df.shape[0], processed_df.shape[0]))
+
+    return Response(processed_df.to_csv(index=False), mimetype='text/csv')
+
+@api.route('/close-outbreaks-nm-ar', methods=['POST'])
+def close_outbreaks_nm_ar():
+    payload = flask.request.data.decode('utf-8')
+    df = pd.read_csv(io.StringIO(payload))
+    flask.current_app.logger.info('DataFrame loaded: %d rows' % df.shape[0])
+
+    state = set(df['State']).pop()
+    standardize_data(df)
+
+    t1 = time()
+    df = close_outbreaks(df)
+    t2 = time()
+
+    # this will go into the lambda logs
+    flask.current_app.logger.info('Closing outbreaks for %s took %s data took %.1f seconds, %d to %d rows' % (
         state, (t2 - t1), df.shape[0], processed_df.shape[0]))
 
     return Response(processed_df.to_csv(index=False), mimetype='text/csv')
---
+++ tests/app/ltc_test.py
@@ -0,0 +1,15 @@
+import pytest
+import filecmp
+import os
+
+import pandas as pd
+
+from app.api.ltc import close_outbreaks
+
+def test_close_outbreaks():
+    test_csv = "tests/app/fixtures/close_outbreak_nm_example.csv"
+    df = pd.read_csv(test_csv)
+    closed = close_outbreaks(df)
+    closed.to_csv("test_nm_closed_outbreak.csv", index=False)
+    assert filecmp.cmp("test_nm_closed_outbreak.csv", "tests/app/fixtures/expected_close_outbreak_nm_example.csv")
+    os.remove("test_nm_closed_outbreak.csv")
